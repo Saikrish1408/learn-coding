7187e3a462a6ef9594e62f2e55feb2b5b9798cb7
Sangeetha-2305 <sangeethaazhagar05@gmail.com>
2025-06-27 21:35:56 +0530

    Prime.java

--- 2305/Prime.java
+++ 2305/Prime.java
@@ -15,7 +15,7 @@ public class Prime {
     }
 
     public static boolean primenumbers(int n) {
-        for (int i = 2; i < Math.sqrt(n); i++) {
+        for (int i = 2; i < n/2; i++) {
             if (n % i == 0) {
                 return false;
 
